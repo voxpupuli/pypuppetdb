d7e5e4ba9e96775f4b1bf184ab747ab3038c51d8
Daniele Sluijters <daniele.sluijters@nedap.com>
2013-08-12 16:49:15 +0200

    pypuppetdb: Onwards to 0.0.3.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ packages = [
 
 setup(
     name='pypuppetdb',
-    version='0.0.2',
+    version='0.0.3',
     author='Daniele Sluijters',
     author_email='daniele.sluijters+pypi@gmail.com',
     packages=packages,
