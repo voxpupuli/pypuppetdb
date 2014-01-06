3711bfd4a71413103b3a024aa49d25708c538c70
Daniele Sluijters <daniele.sluijters@nedap.com>
2014-01-06 15:24:05 +0100

    package: Add 2014 to the copyright.

--- pypuppetdb/package.py
+++ pypuppetdb/package.py
@@ -5,5 +5,5 @@ __version_info__ = ('dev',)  # notest
 __version__ = '.'.join("{0}".format(num) for num in __version_info__)  # notest
 __author__ = 'Daniele Sluijters'
 __license__ = 'Apache 2.0'
-__year__ = '2013'
+__year__ = '2013, 2014'
 __copyright__ = 'Copyright {0} {1}'.format(__year__, __author__)
