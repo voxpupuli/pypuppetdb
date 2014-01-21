deffc7ce59844b3d9183acf9ecd186638a707432
Daniele Sluijters <daniele.sluijters@nedap.com>
2014-01-21 15:35:45 +0100

    tests: Fix license test.

--- tests/test_package.py
+++ tests/test_package.py
@@ -6,6 +6,6 @@ from pypuppetdb.package import (
 def test_package():
     assert __title__ == 'pypuppetdb'
     assert __author__ == 'Daniele Sluijters'
-    assert __license__ == 'Apache 2.0'
+    assert __license__ == 'Apache License 2.0'
     assert __year__ == '2013, 2014'
     assert __copyright__ == 'Copyright {0} {1}'.format(__year__, __author__)
