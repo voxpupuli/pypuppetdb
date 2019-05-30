3606eb8d95008f339838d5db027064b278594623
Tim Meusel <tim@bastelfreak.de>
2019-05-30 17:25:54 +0200

    define the project status as stable

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         "requests >= 2.22.0",
         ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
