7a7856d9ec56de91325c7bf7c62ff25c0241badc
Gabriel Filion <gabster@lelutin.ca>
2020-10-10 19:33:38 -0400

    Add test for creating connection with 'with' statement.

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -4,3 +4,8 @@ import pypuppetdb
 def test_connect_api():
     puppetdb = pypuppetdb.connect()
     assert puppetdb.version == 'v4'
+
+
+def test_connect_with_statement():
+    with pypuppetdb.connect() as puppetdb:
+        assert puppetdb.version == 'v4'
