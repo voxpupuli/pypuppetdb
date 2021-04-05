183963c3e2b585109205ccd6b6550a44b6622656
Greg Dubicki <gdubicki@egnyte.com>
2021-04-05 16:10:13 +0200

    Add PQL test with casting

--- tests/test_api_pql.py
+++ tests/test_api_pql.py
@@ -7,7 +7,64 @@ from pypuppetdb.types import Node, Inventory, Fact
 
 class TestPqlAPI(object):
 
-    def test_pql(self, api):
+    def test_pql_casting(self, api):
+        pql_query = """
+          nodes {
+            facts {
+              name = "operatingsystem" and
+              value = "Debian"
+            }
+          }
+        """
+        pql_body = [
+            {
+                "cached_catalog_status": "not_used",
+                "catalog_environment": "production",
+                "catalog_timestamp": "2016-08-15T11:06:26.275Z",
+                "certname": "greenserver.vm",
+                "deactivated": None,
+                "expired": None,
+                "facts_environment": "production",
+                "facts_timestamp": "2016-08-15T11:06:26.140Z",
+                "latest_report_hash": "4a956674b016d95a7b77c99513ba26e4a744f8d1",
+                "latest_report_noop": False,
+                "latest_report_noop_pending": None,
+                "latest_report_status": "changed",
+                "report_environment": "production",
+                "report_timestamp": "2016-08-15T11:06:18.393Z"
+            },
+            {
+                "cached_catalog_status": "not_used",
+                "catalog_environment": "production",
+                "catalog_timestamp": "2016-08-15T11:06:26.275Z",
+                "certname": "blueserver.vm",
+                "deactivated": None,
+                "expired": None,
+                "facts_environment": "production",
+                "facts_timestamp": "2016-08-15T11:06:26.140Z",
+                "latest_report_hash": "4a956674b016d95a7b77c99513ba26e4a744f8d1",
+                "latest_report_noop": False,
+                "latest_report_noop_pending": None,
+                "latest_report_status": "changed",
+                "report_environment": "production",
+                "report_timestamp": "2016-08-15T11:06:18.393Z"
+            }
+        ]
+        pql_url = 'http://localhost:8080/pdb/query/v4'
+
+        httpretty.enable()
+        httpretty.register_uri(httpretty.GET, pql_url,
+                               body=json.dumps(pql_body))
+
+        nodes = list(api.pql(pql_query))
+
+        assert type(nodes[0]) == Node
+        assert nodes[0].name == "greenserver.vm"
+
+        httpretty.disable()
+        httpretty.reset()
+
+    def test_pql_no_casting(self, api):
         pql_query = """
           nodes[certname] {
             facts {
